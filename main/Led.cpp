3f6341d9b013e8ece0d641a814f077b0f3e048f8
Josiah Chapman <josiah.chapman@gmail.com>
2022-04-23 01:32:15 -0700

    the keyholes have all decided to become WS2811's now, apparently. Why remains to be seen.

--- main/Led.cpp
+++ main/Led.cpp
@@ -5,9 +5,11 @@ namespace keyhole {
 CRGB leds[LED_COUNT] = {0};
 
 void initializeLeds() {
-    FastLED.addLeds<WS2812B, 15, GRB>(leds, 0, LED_SLICE_SIZE);
-    FastLED.addLeds<WS2812B, 2, GRB>(leds, LED_SLICE_SIZE, LED_SLICE_SIZE);
-    FastLED.addLeds<WS2812B, 4, GRB>(leds, 2 * LED_SLICE_SIZE, LED_SLICE_SIZE);
+    FastLED.addLeds<WS2811, 13, GRB>(leds, 0 * LED_SLICE_SIZE, LED_SLICE_SIZE);
+    FastLED.addLeds<WS2811, 12, GRB>(leds, 1 * LED_SLICE_SIZE, LED_SLICE_SIZE);
+    FastLED.addLeds<WS2811, 14, GRB>(leds, 2 * LED_SLICE_SIZE, LED_SLICE_SIZE);
+    FastLED.addLeds<WS2811, 27, GRB>(leds, 3 * LED_SLICE_SIZE, LED_SLICE_SIZE);
+    FastLED.addLeds<WS2811, 26, GRB>(leds, 4 * LED_SLICE_SIZE, LED_SLICE_SIZE);
     FastLED.setMaxPowerInVoltsAndMilliamps(5, LED_MAX_MILIAMPS);
 }
 
